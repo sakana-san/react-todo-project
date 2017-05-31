aff1c257c3327f2eaae1f7cb73503c1e5b163263
sakana-san <sakana@uozakimasaru-no-MacBook-Pro.local>
2017-05-31 12:31:36 +0900

    aslantを修正,propTypesの導入

--- src/js/react-common/react-toggle/app.jsx
+++ src/js/react-common/react-toggle/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 class TodoItem extends React.Component {
   render () {
     const todo = this.props.todo;
     return(
       <li className="p-panel__item">
-        <button className="c-button--toggle" onClick={this.props.onToggle}>toggleボタンです</button>
-        <button className="c-button--close" onClick={this.props.onDelete}></button>
+        <button className="c-button--toggle" type="button" onClick={this.props.onToggle}>toggleボタンです</button>
+        <button className="c-button--close" type="button" onClick={this.props.onDelete}></button>
         <span className={todo.done ? 'is-line-through' : ''}>
           { todo.text }
         </span>
@@ -15,6 +15,12 @@ class TodoItem extends React.Component {
     );
   }
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired
+};
 
 export default class extends React.Component {
   constructor(props) {
@@ -100,7 +106,8 @@ export default class extends React.Component {
           }}
         />
 
-        <button className="c-button"
+        <button
+          className="c-button"
           onClick={ () => {
             this.addTodo({
               text: this.state.text,
